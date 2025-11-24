7b4f155e6020739f5545444e407973d1231a5a91
Florencia Sepúlveda <mari.burgoss@duocuc.cl>
2025-11-24 20:44:26 -0300

    SalmonttApp final

--- src/main/java/util/ArchivoUtil.java
+++ src/main/java/util/ArchivoUtil.java
@@ -3,7 +3,7 @@ package util;
 import java.nio.file.Paths;
 
 public class ArchivoUtil {
-    // Devuelve la ruta completa al archivo dentro de src/main/resources
+
     public static String obtenerRutaDatosEmpleados() {
         return Paths.get("src", "main", "resources", "datos_empleados.txt").toString();
     }
